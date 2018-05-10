53ee7e7dab363ed0789c9a910f974727525613d9
Ahmed Ibrahim <as01220767994@gmail.com>
2018-05-10 23:23:54 +0300

    initial commit

---
+++ app/src/main/java/com/nloops/ntasks/TasksList.java
@@ -0,0 +1,13 @@
+package com.nloops.ntasks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TasksList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tasks_list);
+    }
+}
