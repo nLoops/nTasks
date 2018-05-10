c1579029acd37adb2a69f62105fc125fd7204c3a
Ahmed Ibrahim <as01220767994@gmail.com>
2018-05-11 01:48:56 +0300

    refactor : re-structure the code of the task model
    add a constant values for missing data and assign some annotations

--- app/src/main/java/com/nloops/ntasks/data/Task.java
+++ app/src/main/java/com/nloops/ntasks/data/Task.java
@@ -4,6 +4,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Model that represent single task structure.
@@ -11,55 +12,38 @@ import java.util.ArrayList;
 
 public class Task {
 
+    /*Constants for missing data*/
+    public static final long NO_ID = -1;
+    public static final long NO_DATE = Long.MAX_VALUE;
+
+    private long mID;
+    @NonNull
     private String mTitle;
+    @NonNull
     private String mBody;
-    private int mDate;
+    @NonNull
+    private int mType;
+    @NonNull
     private int mPriorty;
+    @NonNull
+    private long mDate;
+    @Nullable
+    private int mCompleted;
+    @Nullable
     private String mPath;
-    private ArrayList<Todo> mTodos;
+    @Nullable
+    private List<Todo> mTodos;
 
-
-    /**
-     * Public Constructor
-     *
-     * @param title
-     * @param body
-     * @param date
-     * @param priorty
-     * @param path
-     * @param todos
-     */
-    public Task(@NonNull String title, @NonNull String body, @NonNull int date,
-                @NonNull int priorty, @Nullable String path, @Nullable ArrayList<Todo> todos) {
+    public Task(@NonNull String title, @NonNull String body, @NonNull int type, @NonNull int priorty,
+                @NonNull long date, int completed, String path, List<Todo> todo) {
+        this.mID = NO_ID;
         this.mTitle = title;
         this.mBody = body;
-        this.mDate = date;
+        this.mType = type;
         this.mPriorty = priorty;
+        this.mDate = date;
+        this.mCompleted = completed;
         this.mPath = path;
-        this.mTodos = todos;
-    }
-
-    public String getTitle() {
-        return mTitle;
-    }
-
-    public String getBody() {
-        return mBody;
-    }
-
-    public int getDate() {
-        return mDate;
-    }
-
-    public int getPriorty() {
-        return mPriorty;
-    }
-
-    public String getPath() {
-        return mPath;
-    }
-
-    public ArrayList<Todo> getTodos() {
-        return mTodos;
+        this.mTodos = todo;
     }
 }
