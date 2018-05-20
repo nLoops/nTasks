911300c1c81cc759f605680f14fa6048d061ac11
Ahmed Ibrahim <as01220767994@gmail.com>
2018-05-20 05:23:43 +0300

    feat : Add-Edit Fragment UI
    Add mic icon on edittext to allow Speech-to-text,
    add Switch to affect task priorty

--- app/src/main/java/com/nloops/ntasks/addedittasks/TaskDetailFragment.java
+++ app/src/main/java/com/nloops/ntasks/addedittasks/TaskDetailFragment.java
@@ -8,6 +8,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.SwitchCompat;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -33,6 +34,8 @@ public class TaskDetailFragment extends Fragment implements TaskDetailContract.V
     EditText mTitle;
     @BindView(R.id.task_detail_body)
     EditText mBody;
+    @BindView(R.id.task_detail_priority_switch)
+    SwitchCompat mPriortySwitch;
 
     private Task mTask;
 
