d5f933548c8e474519dd3d865eb435417ed5e9a2
Ahmed Ibrahim <as01220767994@gmail.com>
2018-06-07 11:49:54 +0300

    feat : Add public Constructor to extract TODO data from Cursor.

--- app/src/main/java/com/nloops/ntasks/data/Todo.java
+++ app/src/main/java/com/nloops/ntasks/data/Todo.java
@@ -1,12 +1,18 @@
 package com.nloops.ntasks.data;
 
+import android.database.Cursor;
+
+import static com.nloops.ntasks.data.TasksDBContract.*;
+
 /**
  * Model that represent task relative TODOs
  */
 public class Todo {
 
-    private int mID;
+    private long mID;
     private String mTodo;
+    private int mIsCompleted;
+    private int mTaskID;
 
     /**
      * public Constructor.
@@ -14,26 +20,33 @@ public class Todo {
      * @param id
      * @param
      */
-    public Todo(int id, String todo) {
+    public Todo(int id, String todo, int isCompleted) {
         this.mID = id;
         this.mTodo = todo;
+        this.mIsCompleted = isCompleted;
+
+    }
+
+    public Todo(Cursor cursor) {
+        this.mID = getColumnLong(cursor, TodoEntry._ID);
+        this.mTodo = getColumnString(cursor, TodoEntry.COLUMN_NAME_TODO);
+        this.mTaskID = getColumnInt(cursor, TodoEntry.COLUMN_NAME_TASK_ID);
+        this.mIsCompleted = getColumnInt(cursor, TodoEntry.COLUMN_NAME_COMPLETE);
+
     }
 
     /**
      * Public Constructor.
      *
-     * @param todo
+     * @param todoString the body of single todoString.
      */
-    public Todo(String todo) {
-        this.mTodo = todo;
+    public Todo(String todoString, int isCompleted) {
+        this.mTodo = todoString;
+        this.mIsCompleted = isCompleted;
     }
 
-    public int getID() {
+    public long getID() {
         return mID;
-    }
-
-    public void setID(int mID) {
-        this.mID = mID;
     }
 
     public String getTodo() {
@@ -44,5 +57,23 @@ public class Todo {
         this.mTodo = mTodo;
     }
 
+    public int getIsCompleted() {
+        return mIsCompleted;
+    }
 
+    public boolean isComplete() {
+        return mIsCompleted == TodoEntry.STATE_COMPLETED;
+    }
+
+    public void setIsCompleted(int mIsCompleted) {
+        this.mIsCompleted = mIsCompleted;
+    }
+
+    public int getTaskID() {
+        return mTaskID;
+    }
+
+    public void setTaskID(int mTaskID) {
+        this.mTaskID = mTaskID;
+    }
 }
