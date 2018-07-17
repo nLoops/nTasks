01e87b4752fbc29e678e9989246b33012e3b263d
Ahmed Ibrahim <as01220767994@gmail.com>
2018-07-17 23:14:26 +0300

    org: handling OnDrawerItem Click Listener Interface.

--- app/src/main/java/com/nloops/ntasks/UI/DrawerMenuItem.java
+++ app/src/main/java/com/nloops/ntasks/UI/DrawerMenuItem.java
@@ -4,8 +4,6 @@ import android.content.Context;
 import android.support.v4.widget.DrawerLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import com.mindorks.placeholderview.annotations.Click;
 import com.mindorks.placeholderview.annotations.Layout;
 import com.mindorks.placeholderview.annotations.Resolve;
@@ -13,58 +11,58 @@ import com.mindorks.placeholderview.annotations.View;
 import com.nloops.ntasks.R;
 
 /**
- * This Class will hold {@link DrawerLayout} items,
- * we can here define a interface with listener to items clicks
+ * This Class will hold {@link DrawerLayout} items, we can here define a interface with listener to
+ * items clicks
  */
 @Layout(R.layout.drawer_item)
 public class DrawerMenuItem {
 
-    public static final int DRAWER_MENU_ITEM_CALENDER_VIEW = 1;
+  public static final int DRAWER_MENU_ITEM_CALENDER_VIEW = 1;
 
-    private int mMenuPosition;
-    private Context mContext;
-    private DrawerCallBack mCallBack;
+  private int mMenuPosition;
+  private Context mContext;
+  private DrawerCallBack mCallBack;
 
-    @View(R.id.itemNameTxt)
-    private TextView itemNameTxt;
+  @View(R.id.itemNameTxt)
+  private TextView itemNameTxt;
 
-    @View(R.id.itemIcon)
-    private ImageView itemIcon;
+  @View(R.id.itemIcon)
+  private ImageView itemIcon;
 
-    public DrawerMenuItem(Context context, int menuPosition) {
-        mContext = context;
-        mMenuPosition = menuPosition;
+  public DrawerMenuItem(Context context, int menuPosition) {
+    this.mContext = context;
+    this.mMenuPosition = menuPosition;
+  }
+
+  @Resolve
+  private void onResolved() {
+    switch (mMenuPosition) {
+      case DRAWER_MENU_ITEM_CALENDER_VIEW:
+        itemNameTxt.setText(mContext.getString(R.string.drawer_calender_view));
+        itemIcon.setImageDrawable(mContext.getResources().
+            getDrawable(R.drawable.ic_empty_calender_small));
+        break;
     }
+  }
 
-    @Resolve
-    private void onResolved() {
-        switch (mMenuPosition) {
-            case DRAWER_MENU_ITEM_CALENDER_VIEW:
-                itemNameTxt.setText(mContext.getString(R.string.drawer_calender_view));
-                itemIcon.setImageDrawable(mContext.getResources().
-                        getDrawable(R.drawable.ic_empty_calender_small));
-                break;
+  @Click(R.id.mainView)
+  private void onMenuItemClick() {
+    switch (mMenuPosition) {
+      case DRAWER_MENU_ITEM_CALENDER_VIEW:
+        if (mCallBack != null) {
+          mCallBack.onCaldendarViewSelected();
         }
+        break;
     }
+  }
 
-    @Click(R.id.mainView)
-    private void onMenuItemClick() {
-        switch (mMenuPosition) {
-            case DRAWER_MENU_ITEM_CALENDER_VIEW:
-                Toast.makeText(mContext, "Calender View", Toast.LENGTH_LONG).show();
-                if (mCallBack != null) mCallBack.onCaldenderViewSelected();
-                break;
-        }
-    }
+  public void setDrawerCallBack(DrawerCallBack callBack) {
+    mCallBack = callBack;
+  }
 
-    public void setDrawerCallBack(DrawerCallBack callBack) {
-        mCallBack = callBack;
-    }
+  public interface DrawerCallBack {
 
-    public interface DrawerCallBack {
-        void onCaldenderViewSelected();
-
-    }
-
+    void onCaldendarViewSelected();
+  }
 
 }
