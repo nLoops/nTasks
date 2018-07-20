fbd2b71c2c60eed395c68e33b79c299ccb9d94ad
Ahmed Ibrahim <as01220767994@gmail.com>
2018-07-20 23:46:34 +0300

    fix: The crash that happens when first add home widget to get data.

--- app/src/main/java/com/nloops/ntasks/widgets/TasksListWidget.java
+++ app/src/main/java/com/nloops/ntasks/widgets/TasksListWidget.java
@@ -4,9 +4,7 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 import android.widget.RemoteViews;
-
 import com.nloops.ntasks.R;
 
 /**
@@ -14,37 +12,38 @@ import com.nloops.ntasks.R;
  */
 public class TasksListWidget extends AppWidgetProvider {
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.tasks_list_widget);
-        Intent intent = new Intent(context, ListViewService.class);
-        views.setRemoteAdapter(R.id.tasks_list_widget_lv, intent);
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
-    }
+  static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+      int appWidgetId) {
+    // Construct the RemoteViews object
+    RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.tasks_list_widget);
+    Intent intent = new Intent(context, ListViewService.class);
+    views.setRemoteAdapter(R.id.tasks_list_widget_lv, intent);
+    // Instruct the widget manager to update the widget
+    appWidgetManager.updateAppWidget(appWidgetId, views);
+  }
 
-    @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them
-        WidgetIntentService.startActionChangeList(context);
-    }
+  @Override
+  public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+    // There may be multiple widgets active, so update all of them
+    appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.tasks_list_widget_lv);
+    updateTasksWidgets(context, appWidgetManager, appWidgetIds);
+  }
 
-    public static void updateTasksWidgets(Context context, AppWidgetManager manager,
-                                          int[] appWidgetsIds) {
-        for (int appWidgetId : appWidgetsIds) {
-            updateAppWidget(context, manager, appWidgetId);
-        }
+  public static void updateTasksWidgets(Context context, AppWidgetManager manager,
+      int[] appWidgetsIds) {
+    for (int appWidgetId : appWidgetsIds) {
+      updateAppWidget(context, manager, appWidgetId);
     }
+  }
 
-    @Override
-    public void onEnabled(Context context) {
-        // Enter relevant functionality for when the first widget is created
-    }
+  @Override
+  public void onEnabled(Context context) {
+    // Enter relevant functionality for when the first widget is created
+  }
 
-    @Override
-    public void onDisabled(Context context) {
-        // Enter relevant functionality for when the last widget is disabled
-    }
+  @Override
+  public void onDisabled(Context context) {
+    // Enter relevant functionality for when the last widget is disabled
+  }
 }
 
