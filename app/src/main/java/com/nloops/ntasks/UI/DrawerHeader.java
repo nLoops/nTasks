b8f3a4b92e31d4edb30f89032e895b3eb3333fda
Ahmed Ibrahim <as01220767994@gmail.com>
2018-08-17 15:47:35 +0300

    org: Add E-Mail field to DrawerHeader

--- app/src/main/java/com/nloops/ntasks/UI/DrawerHeader.java
+++ app/src/main/java/com/nloops/ntasks/UI/DrawerHeader.java
@@ -1,8 +1,6 @@
 package com.nloops.ntasks.UI;
 
 import android.content.Context;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.mindorks.placeholderview.annotations.Layout;
@@ -10,37 +8,40 @@ import com.mindorks.placeholderview.annotations.NonReusable;
 import com.mindorks.placeholderview.annotations.Resolve;
 import com.mindorks.placeholderview.annotations.View;
 import com.nloops.ntasks.R;
+import com.nloops.ntasks.login.LocalUser;
+import com.nloops.ntasks.utils.SharedPreferenceHelper;
 
 /**
  * From Library Author Tutorial
  *
- * @NonReusable is annotation in PlaceHolderView,
- * to be used in cases where we want to release all the resources and references
- * if removed from the list and won’t use the same object in addView() method in PlaceHolderView.
+ * @NonReusable is annotation in PlaceHolderView, to be used in cases where we want to release all
+ * the resources and references if removed from the list and won’t use the same object in addView()
+ * method in PlaceHolderView.
  * @layout is used to bind the layout with this class
  * @View is used to bind the views in this layout we want to refer to
- * @Resolve is used to operate on the view references obtained from @View,
- * in short if we want to define any operation on the views it should be put in a method
- * and annotated with @Resolve
+ * @Resolve is used to operate on the view references obtained from @View, in short if we want to
+ * define any operation on the views it should be put in a method and annotated with @Resolve
  */
 @NonReusable
 @Layout(R.layout.drawer_header)
 public class DrawerHeader {
 
-    @View(R.id.profileImageView)
-    private ImageView mImageView;
-    @View(R.id.nameTxt)
-    private TextView mUserName;
-    private final Context mContext;
+  private final Context mContext;
+  @View(R.id.profileImageView)
+  private ImageView mImageView;
+  @View(R.id.nameTxt)
+  private TextView mUserName;
+  @View(R.id.email_address)
+  private TextView mEmailAddress;
 
-    public DrawerHeader(Context context) {
-        this.mContext = context;
-    }
+  public DrawerHeader(Context context) {
+    this.mContext = context;
+  }
 
-    @Resolve
-    private void onResolved() {
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(mContext);
-        String userName = preferences.getString(mContext.getString(R.string.current_user_display_name), "");
-        mUserName.setText(userName);
-    }
+  @Resolve
+  private void onResolved() {
+    LocalUser user = SharedPreferenceHelper.getInstance(mContext).getUserInfo();
+    mUserName.setText(user.name);
+    mEmailAddress.setText(user.email);
+  }
 }
